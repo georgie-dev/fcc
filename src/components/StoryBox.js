44ac741ff4670f4e6dd3216766840b53cbdb5e1d
georgie-dev <mangergeorgepraise@gmail.com>
2024-12-04 10:42:17 +0100

    initial

--- src/components/StoryBox.js
+++ src/components/StoryBox.js
@@ -12,7 +12,7 @@ const StoryBox = ({ data }) => {
       <div className=" rounded-full w-6 h-6 flex text-gray-200 items-center justify-center bg-zinc-700 absolute top-2 right-2 text-xs">
         {data.number}
       </div>
-      <div className="bg-white absolute bottom-2 left-6 md:left-14 lg:left-[72px] rounded-2xl flex font-semibold items-center gap-1 px-2 py-[2px] text-sm text-zinc-600">
+      <div className="bg-white absolute bottom-2 left-7 md:left-14 lg:left-[72px] rounded-2xl flex font-semibold items-center gap-1 px-2 py-[2px] text-sm text-zinc-600">
         <IoPlay />
         <small className="font-bold">{Number(data.plays).toLocaleString()}</small>
       </div>
